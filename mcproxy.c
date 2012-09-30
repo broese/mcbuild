20b51318b6f8372a8a1380a8c58d1e465a4fbc1d
Eduard Bröse <ed.broese@gmx.de>
2012-09-30 01:49:28 +0000

    * simplyfy the mcproxy main pump

--- mcproxy.c
+++ mcproxy.c
@@ -25,61 +25,65 @@ typedef struct {
 } buffer_t;
 */
 
-int handle_server(int sfd, uint32_t ip, uint16_t port, FILE **csock, FILE **msock) {
+int handle_server(int sfd, uint32_t ip, uint16_t port, int *cs, int *ms) {
 
     //// Handle Client-Side Connection
 
     // accept connection from the client side
     struct sockaddr_in cadr;
     int size = sizeof(cadr);
-    int cl = accept(sfd, (struct sockaddr *)&cadr, &size);
-    if (cl < 0) LH_ERROR(0, "Failed to accept the client-side connection",strerror(errno));
+    *cs = accept(sfd, (struct sockaddr *)&cadr, &size);
+    if (*cs < 0) LH_ERROR(0, "Failed to accept the client-side connection",strerror(errno));
     printf("Accepted from %s:%d\n",inet_ntoa(cadr.sin_addr),ntohs(cadr.sin_port));
 
     // set non-blocking mode on the client socket
-    if (fcntl(cl, F_SETFL, O_NONBLOCK) < 0) {
-        close(cl);
+    if (fcntl(*cs, F_SETFL, O_NONBLOCK) < 0) {
+        close(*cs);
         LH_ERROR(0, "Failed to set non-blocking mode for the client",strerror(errno));
     }
-
-    // make it to a FILE *
-    *csock = fdopen(cl, "r+");
-    if (! *csock) {
-        close(cl);
-        LH_ERROR(0, "Failed to fdopen the client socket",strerror(errno));
-    }
-
-
 
     //// Handle Server-Side Connection
 
     // open connection to the remote server
-    int ms = sock_client_ipv4_tcp(ip, port);
-    if (ms < 0) {
-        fclose(*csock);
-        *csock = NULL;
+    *ms = sock_client_ipv4_tcp(ip, port);
+    if (*ms < 0) {
+        close(*cs);
         LH_ERROR(0, "Failed to open the client-side connection",strerror(errno));
     }
 
     // set non-blocking mode
-    if (fcntl(ms, F_SETFL, O_NONBLOCK) < 0) {
-        fclose(*csock);
-        *csock = NULL;
-        close(ms);
+    if (fcntl(*ms, F_SETFL, O_NONBLOCK) < 0) {
+        close(*cs);
+        close(*ms);
         LH_ERROR(0, "Failed to set non-blocking mode for the server",strerror(errno));
     }
 
-    // make it to a FILE *
-    *msock = fdopen(ms, "r+");
-    if (! *msock) {
-        fclose(*csock);
-        *csock = NULL;
-        close(ms);
-        LH_ERROR(0, "Failed to fdopen the server-side socket",strerror(errno));
-    }
-
     return 1;
+}
 
+int pumpdata(int src, int dst, uint8_t **sbuf, ssize_t *slen, int is_client) {
+    int res = evfile_read(src, sbuf, slen, 1048576);
+
+    switch (res) {
+        case EVFILE_OK:
+        case EVFILE_WAIT: {
+            uint8_t *buf = *sbuf;
+            ssize_t len = *slen;
+            while (len > 0) {
+                hexdump(buf,len);
+                printf("sending %d bytes to %s\n",len,is_client?"client":"server");
+                ssize_t sent = write(dst,buf,len);
+                printf("sent %d bytes to %s\n",sent,is_client?"client":"server");
+                buf += sent;
+                len -= sent;
+            }
+            *slen = 0;
+            return 0;
+        }         
+        case EVFILE_ERROR:
+        case EVFILE_EOF:
+            return -1;
+    }
 }
 
 int proxy_pump(uint32_t ip, uint16_t port) {
@@ -104,13 +108,14 @@ int proxy_pump(uint32_t ip, uint16_t port) {
     // add the server to listen for new client connections
     pollarray_add(&pa, &sg, ss, MODE_R, NULL);
 
-    FILE *csock = NULL, *msock = NULL;
+    int cs=-1, ms=-1;
 
     // growable buffers
     ARRAY(uint8_t,crbuf,crlen);
     //ARRAY(uint8_t,cwbuf,cwlen);
     ARRAY(uint8_t,srbuf,srlen);
     //ARRAY(uint8_t,swbuf,swlen);
+
 
     // main pump
     int stay = 1, i;
@@ -125,87 +130,33 @@ int proxy_pump(uint32_t ip, uint16_t port) {
                mg.rn,mg.wn,mg.en);
 
         // handle connection requests from the client side
-        if (sg.rn > 0 && csock == NULL)
-            if (handle_server(pa.p[sg.r[0]].fd, ip, port, &csock, &msock)) {
+        if (sg.rn > 0 && cs == -1)
+            if (handle_server(pa.p[sg.r[0]].fd, ip, port, &cs, &ms)) {
                 printf("%s:%d - handle_server successful cs=%d ms=%d\n",
-                       __func__,__LINE__, fileno(csock), fileno(msock));
+                       __func__,__LINE__, cs, ms);
                 
                 // handle_server was able to accept the client connection and
                 // also open the server-side connection, we need to add these
                 // new sockets to the groups cg and mg respectively
-                pollarray_add_file(&pa, &cg, csock, MODE_R, NULL);
-                pollarray_add_file(&pa, &mg, msock, MODE_R, NULL);
+                pollarray_add(&pa, &cg, cs, MODE_R, NULL);
+                pollarray_add(&pa, &mg, ms, MODE_R, NULL);
             }
-        
+
         if (cg.rn > 0) {
-            printf("%s:%d\n",__func__,__LINE__);
-            // incoming data from the client side
-            FILE * fd = pa.files[cg.r[0]];
-            printf("%s:%d fd=%d fd=%08x\n",__func__,__LINE__,fd);
-            printf("%s:%d fd=%d\n",__func__,__LINE__,fileno(fd));
-            int res = evfile_read(fileno(fd), &crbuf, &crlen, 1048576);
-
-            switch (res) {
-                case EVFILE_OK:
-                case EVFILE_WAIT: {
-                    uint8_t *buf = crbuf;
-                    ssize_t len = crlen;
-                    while (len > 0) {
-                        hexdump(buf,len);
-                        printf("%s:%d len=%d\n",__func__,__LINE__,len);
-                        printf("sending %d bytes to server\n",len);
-                        ssize_t sent = write(fileno(msock),buf,len);
-                        printf("sent %d bytes to server\n",sent);
-                        buf += sent;
-                        len -= sent;
-                    }
-                    crlen = 0;
-                    break;
-                }         
-                case EVFILE_ERROR:
-                    printf("Error occurred on fd=%d\n",fileno(fd));
-                case EVFILE_EOF:
-                    printf("Closing client connection for fd=%d\n",fileno(fd));
-                    crlen = 0;
-                    pollarray_remove_file(&pa, fd);
-                    break;
+            if (pumpdata(cs, ms, &crbuf, &crlen, 1)) {
+                pollarray_remove(&pa, cs);
+                pollarray_remove(&pa, ms);
+                cs = ms = -1;
             }
         }
-            
+               
         if (mg.rn > 0) {
-            printf("%s:%d\n",__func__,__LINE__);
-            // incoming data from the client side
-            FILE * fd = pa.files[mg.r[0]];
-            printf("%s:%d fd=%d fd=%08x\n",__func__,__LINE__,fd);
-            printf("%s:%d fd=%d\n",__func__,__LINE__,fileno(fd));
-            int res = evfile_read(fileno(fd), &srbuf, &srlen, 1048576);
-
-            switch (res) {
-                case EVFILE_OK:
-                case EVFILE_WAIT: {
-                    uint8_t *buf = srbuf;
-                    ssize_t len = srlen;
-                    while (len > 0) {
-                        hexdump(buf,len);
-                        printf("%s:%d len=%d\n",__func__,__LINE__,len);
-                        printf("sending %d bytes to client\n",len);
-                        ssize_t sent = write(fileno(csock),buf,len);
-                        printf("sent %d bytes to client\n",sent);
-                        buf += sent;
-                        len -= sent;
-                    }
-                    srlen = 0;
-                    break;
-                }         
-                case EVFILE_ERROR:
-                    printf("Error occurred on fd=%d\n",fileno(fd));
-                case EVFILE_EOF:
-                    printf("Closing server connection for fd=%d\n",fileno(fd));
-                    srlen = 0;
-                    pollarray_remove_file(&pa, fd);
-                    break;
+            if (pumpdata(ms, cs, &srbuf, &srlen, 1)) {
+                pollarray_remove(&pa, cs);
+                pollarray_remove(&pa, ms);
+                cs = ms = -1;
             }
-        }
+        }               
     }
 }
 
