f21263cb8afc4d66ef58367d1f1644c8072a32ce
Eduard Bröse <ed.broese@gmx.de>
2015-02-21 23:41:55 +0100

    export chat_message function for other modules and in order to make it available everywhere in mcp_game.c

--- mcp_game.h
+++ mcp_game.h
@@ -1,5 +1,7 @@
 #pragma once
 
+static void chat_message(const char *str, MCPacketQueue *q, const char *color, int pos);
+
 void gm_packet(MCPacket *pkt, MCPacketQueue *tq, MCPacketQueue *bq);
 void gm_reset();
 void gm_async(MCPacketQueue *sq, MCPacketQueue *cq);
