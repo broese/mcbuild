c50d36ff26c7fd0756c56650aba77c73be7580dd
Eduard Bröse <ed.broese@gmx.de>
2015-08-15 16:23:30 +0200

    structs for default argument values and the data types for the argument parsing. This probably will need to be split and moved to other place later

--- mcp_arg.h
+++ mcp_arg.h
@@ -36,3 +36,27 @@ int mcparg_parse_size(char **words, int argpos, char *reply, int *sx, int *sz, i
 // macro to produce a NULL-terminated char ** - compatible list of words
 //FIXME - possible to use a "const char **" everywhere?
 #define WORDLIST(...) (char*[]) { __VA_ARGS__, NULL }
+
+// a struct containing all relevant values from gamestate that may be needed
+// as default values in the argument parsing. We are passing these values through
+// this struct in order to isolate mcparg module from the gamestate module
+typedef struct {
+    int32_t px,py,pz;   // player position (in block units)
+    int     pd;         // player's look direction
+    bid_t   mat;        // material currently held by the player
+    bid_t   mat2;       // material in the next slot than what player currently holds
+    //TODO: current mask
+} arg_defaults;
+
+typedef struct {
+    int32_t x,y,z;
+} off3_t;               // data type - block offset/coordinate
+
+typedef struct {
+    int32_t x,y,z;
+} size3_t;              // data type - block size
+
+typedef struct {
+    off3_t  pos;
+    int     dir;
+} pivot_t;              // data type - pivot position and orientation
