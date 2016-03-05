e91c3831ecd632812d50b9c94e431c19fbcafbfa
Eduard Bröse <ed.broese@gmx.de>
2016-03-05 12:12:44 +0100

    Bump version to 1.0.1

--- mcproxy.c
+++ mcproxy.c
@@ -1102,7 +1102,7 @@ int parse_args(int ac, char **av) {
         { printf("Failed to create directory '%s'\n", #name ); return 1; }
 
 int main(int ac, char **av) {
-    printf("MCBuild 1.0 for MC Protocol 1.8\n");
+    printf("MCBuild 1.0.1 for MC Protocol 1.8\n");
 
     // create directories if they don't exist yet
     MKDIR(saved);
