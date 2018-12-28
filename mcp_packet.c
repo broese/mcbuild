7083607687bf22e5d0f625a5a9a3de4b625b2b7c
harbaughfan <thygermc@gmail.com>
2018-12-28 08:13:01 -0600

    0x43 SP_SetExperience is confirmed

--- mcp_packet.c
+++ mcp_packet.c
@@ -1961,7 +1961,7 @@ const static packet_methods SUPPORT_1_13_2[2][MAXPACKETTYPES] = {
         SUPPORT_    (0x40,SP_AttachEntity),
         SUPPORT_    (0x41,SP_EntityVelocity),
         SUPPORT_    (0x42,SP_EntityEquipment),
-        SUPPORT_    (0x43,SP_SetExperience),
+        SUPPORT_DD  (0x43,SP_SetExperience,_1_8_1),
         SUPPORT_    (0x44,SP_UpdateHealth),
         SUPPORT_    (0x45,SP_ScoreboardObjective),
         SUPPORT_    (0x46,SP_SetPassengers),
